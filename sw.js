b967f56b1da729fc676823615e118179d142b4fc
meusvita <coolbilal99@gmail.com>
2026-06-05 20:28:20 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'expendr-v1';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(['/']))
+  );
+});
+
+self.addEventListener('activate', e => {
+  self.clients.claim();
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
